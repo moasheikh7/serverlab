fc6081b0f23757c2ad1ca801673a774ddbcf1b87
moasheikh7 <moasheikh7@gmail.com>
2026-06-25 23:24:15 +0300

    Add 500 Error Detection

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-grep -E "404|408" /var/log/apache2/access.log > ~/admin.log
+grep -E "404|408|500" /var/log/apache2/access.log > ~/admin.log
 echo "log checked on: $(date)" >> ~/admin.log
 
