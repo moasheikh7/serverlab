8523606aeced3bd943e7cb1cdf021872ed5a2b55
moasheikh7 <moasheikh7@gmail.com>
2026-06-27 14:00:03 +0300

    Add repeated IP detection

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-grep -E "404|408|500" /var/log/apache2/access.log > ~/admin.log
-echo "log checked on: $(date)" >> ~/admin.log
+# Filter suspicious HTTP error codes
+grep -E "404|408|500|401" /var/log/apache2/access.log > ~/admin.log
+
+# Show IPs appearing more than once in the access log
+echo "---Repeated IPs___" >> ~/admin.log
+awk '{print $1}' /var/log/apache2/access.log | sort | uniq -c | sort -rn | awk '$1 > 1' >> ~/admin.log
+
+# Timestamp when this script last ran
+echo "Log checked on: $(date)" >> ~/admin.log
 
